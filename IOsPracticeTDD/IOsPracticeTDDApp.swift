37891b52ec3afd0c612b3ba3fee649d3b7f6b1c1
hovhannes <johnnysc091@gmail.com>
2024-11-27 20:23:17 +0900

    Initial Commit

---
+++ IOsPracticeTDD/IOsPracticeTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOsPracticeTDDApp.swift
+//  IOsPracticeTDD
+//
+//  Created by Hovhannes on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct IOsPracticeTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
