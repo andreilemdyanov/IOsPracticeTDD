effb9bd7ffbe804dab53a34de881718308d161a9
hovhannes <johnnysc091@gmail.com>
2024-12-02 20:14:54 +0900

    task002 empty fun test

--- IOsPracticeTDDTests/Task002EmptyFunTest.swift
+++ IOsPracticeTDDTests/Task002EmptyFunTest.swift
@@ -2,7 +2,7 @@
 //  IOsPracticeTDDTests.swift
 //  IOsPracticeTDDTests
 //
-//  Created by Hovhannes on 27.11.2024.
+//  Created by Hovhannes on 2.12.2024.
 //
 
 import XCTest
@@ -10,27 +10,28 @@ import XCTest
 
 final class Task002EmptyFunTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTask002EmptyFun() throws {
+        let analytics: FakeAnalytics = FakeAnalyticsImpl()
+        let viewModel: OnboardingViewModel = OnboardingViewModel(analytics: analytics)
+        viewModel.onboardingIsShown()
+        analytics.assertOnBoardingEventCalled()
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+private protocol FakeAnalytics : Analytics {
+    
+    func assertOnBoardingEventCalled()
+}
+
+private class FakeAnalyticsImpl : FakeAnalytics {
+    
+    private var sendOnboardingEventCalled = false
+    
+    func sendOnboardingEvent() {
+        sendOnboardingEventCalled = true
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func assertOnBoardingEventCalled() {
+        XCTAssertTrue(sendOnboardingEventCalled)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
